a80ab1f299a22cfc492998b9043c1cd132f54ae2
CodeQueen-AI <codeq209@gmail.com>
2026-01-28 15:38:24 +0500

    update

---
+++ 07-Return Statement/index.js
@@ -0,0 +1,14 @@
+// Return Statement
+function add(a, b) {
+    return a + b; 
+}
+
+let sum = add(5, 3);
+console.log("Sum:", sum); 
+
+// Without return
+function greet(name) {
+    console.log("Hello " + name);
+}
+let result = greet("Sumbal"); 
+console.log("Result:", result);
