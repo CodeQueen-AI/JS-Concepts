d1f1aa4aa28446f3a118e694b1501a84560fdd12
CodeQueen-AI <codeq209@gmail.com>
2026-01-28 15:20:04 +0500

    update

---
+++ 05-Conditional Statements/index.js
@@ -0,0 +1,41 @@
+// If - Else
+let age = 18;
+
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}
+
+// Else If
+let marks = 75;
+
+if (marks >= 90) {
+    console.log("Grade A");
+} else if (marks >= 75) {
+    console.log("Grade B");
+} else if (marks >= 50) {
+    console.log("Grade C");
+} else {
+    console.log("Grade F");
+}
+
+// Switch
+let day = 3;
+
+switch(day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    default:
+        console.log("Other day");
+}
