96887d460105a0904125f005108d3c5e0016e91b
CodeQueen-AI <codeq209@gmail.com>
2026-01-28 15:09:57 +0500

    update

---
+++ 02-DataTypes/index.js
@@ -0,0 +1,31 @@
+// Number
+let age = 25;
+console.log("Age:", age);
+
+// String
+let name = "Sumbal";
+console.log("Name:", name);
+
+// Boolean
+let isStudent = true;
+console.log("Is Student:", isStudent);
+
+// Null
+let emptyValue = null;
+console.log("Empty Value:", emptyValue);
+
+// Undefined
+let notDefined;
+console.log("Not Defined:", notDefined);
+
+// Symbol
+let sym = Symbol("id");
+console.log("Symbol:", sym);
+
+// Object
+let person = { name: "Sumbal", age: 25 };
+console.log("Person Object:", person);
+
+// Array
+let numbers = [1, 2, 3, 4, 5];
+console.log("Numbers Array:", numbers);
