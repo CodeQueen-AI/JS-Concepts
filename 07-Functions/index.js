4eaa824cdc39040c6a55c630e3712679df47dcc1
CodeQueen-AI <codeq209@gmail.com>
2026-01-29 20:31:53 +0500

    update

---
+++ 07-Functions/index.js
@@ -0,0 +1,12 @@
+// Function Declaration
+function greet() {
+  console.log("Hello World");
+}
+greet();
+
+// Function Expression
+const greet = function () {
+  console.log("Hello World");
+};
+greet();
+
