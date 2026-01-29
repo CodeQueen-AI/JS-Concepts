c2d4e5ffb2e5262a8f41324aaefbbbf6c2feb52d
CodeQueen-AI <codeq209@gmail.com>
2026-01-29 20:34:55 +0500

    update

--- 08-Scope/index.js
+++ 08-Scope/index.js
@@ -1,29 +1,8 @@
 // Global Scope
-let globalVar = "I am global";
-
-function testScope() {
-    console.log(globalVar); // Access global variable
-
-    // Local Scope
-    let localVar = "I am local";
-    console.log(localVar);
+let message = "I am global";
+function showMessage() {
+  console.log(message);
 }
 
-testScope();
-// console.log(localVar); // Error: localVar is not defined
-
-// Block Scope
-if (true) {
-    let blockVar = "I am block-scoped";
-    const blockConst = "Also block-scoped";
-    console.log(blockVar);    
-    console.log(blockConst); 
-}
-// console.log(blockVar);    // Error
-// console.log(blockConst);  // Error
-
-// var is not block-scoped
-if (true) {
-    var notBlockVar = "I am not block-scoped";
-}
-console.log(notBlockVar); // Works var ignores block
+showMessage();
+console.log(message);
