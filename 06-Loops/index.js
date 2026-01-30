ec5e82636ccf818ba10782e202207434c8c41323
CodeQueen-AI <codeq209@gmail.com>
2026-01-30 15:39:37 +0500

    update

---
+++ 06-Loops/index.js
@@ -0,0 +1,21 @@
+// 1. For Loop
+console.log("For Loop Example:")
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
+
+// 2. While Loop
+console.log("While Loop Example:")
+let j = 1
+while (j <= 5) {
+    console.log(j)
+    j++
+}
+
+// 3. Do-While Loop
+console.log("Do-While Loop Example:")
+let k = 1
+do {
+    console.log(k)
+    k++
+} while (k <= 5)
